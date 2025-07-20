33eb8a6d98f32325702de1839408d85b5d549a5c
Anna <azeleranskaya@gmail.com>
2025-07-20 16:50:33 +0300

    homework 26-1 done

--- catalog/views.py
+++ catalog/views.py
@@ -1,15 +1,43 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.urls import reverse_lazy
 from django.views.generic import ListView, TemplateView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
+from catalog.forms import ProductForm
 from catalog.models import Product
 
 
 class HomeTemplateView(TemplateView):
     template_name = 'catalog/home.html'
 
+
 class ProductListView(ListView):
     model = Product
+
+
+class ProductDetailView(DetailView):
+    model = Product
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy('catalog:product_list')
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'catalog/product_form.html'
+    success_url = reverse_lazy('catalog:product_list')
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    template_name = 'catalog/product_confirm_delete.html'
+    success_url = reverse_lazy('catalog:product_list')
 
 
 class ContactsTemplateView(TemplateView):
@@ -21,17 +49,3 @@ class ContactsTemplateView(TemplateView):
             name = request.POST.get('name')
             return HttpResponse(f'{name}, Ваше сообщение отправлено!')
         return render(request, 'catalog/contacts.html')
-
-# def contacts(request):
-#     if request.method == 'POST':
-#         name = request.POST.get('name')
-#         return HttpResponse(f'{name}, Ваше сообщение отправлено!')
-#     return render(request, 'contacts.html')
-
-# def product_details(request, pk):
-#     product = Product.objects.get(id=pk)
-#     context = {'product': product}
-#     return render(request, 'product_detail.html', context)
-
-class ProductDetailView(DetailView):
-    model = Product
