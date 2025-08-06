1322b161bedf937fadb9c9e8f47e4c46c76d412f
Anna <azeleranskaya@gmail.com>
2025-08-06 17:45:44 +0300

    homework-29 done

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,10 +7,12 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path('home/', views.HomeTemplateView.as_view(), name='home'),
     path('contacts/', views.ContactsTemplateView.as_view(), name='contacts'),
+    path('category_list/', views.CategoryListView.as_view(), name='category_list'),
     path('product_list/', views.ProductListView.as_view(), name='product_list'),
     path('products/new/', views.ProductCreateView.as_view(), name='product_create'),
     path('products/<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
     path('products/<int:pk>/edit/', views.ProductUpdateView.as_view(), name='product_update'),
     path('products/<int:pk>/delete/', views.ProductDeleteView.as_view(), name='product_delete'),
     path('products/<int:pk>/unpublish/', views.ProductUnpublishView.as_view(), name='product_unpublish'),
+    path('product_list/category/<int:category_id>/', views.ProductByCategoryListView.as_view(), name='product_by_cat_list'),
 ]
--- config/settings.py
+++ config/settings.py
@@ -147,10 +147,18 @@ LOGIN_REDIRECT_URL = 'catalog:home'
 LOGOUT_REDIRECT_URL = 'catalog:home'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.yandex.ru'
-EMAIL_PORT = 465
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
-EMAIL_HOST_USER = 'zele-skypro@yandex.ru'
-EMAIL_HOST_PASSWORD = 'wqjhhmzysgyezefm'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_USE_TLS = True if os.getenv('EMAIL_USE_TLS') == 'True' else False
+EMAIL_USE_SSL = True if os.getenv('EMAIL_USE_SSL') == 'True' else False
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+CACHE_ENABLED = True
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.redis.RedisCache',
+        'LOCATION': os.getenv('LOCATION'),
+    }
+}
